7ef96e22172c84b8ed4dd21c93c469097c09ed93
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-05-20 19:23:06 +0200

    fixed path to website in ALLOWED_HOSTS

--- tasty_ideas_proj/settings.py
+++ tasty_ideas_proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # "django-insecure-8fhj23)@ng=dq@-w4c*%1=$80!p@(t!*380f^4l-3-3+iun3kz"
-SECRET_KEY = os.environ.get["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get["DJANGO_SECRET_KEY", "django-insecure-8fhj23)@ng=dq@-w4c*%1=$80!p@(t!*380f^4l-3-3+iun3kz"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #?????????????????
@@ -34,7 +34,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'https://tasty-ideas-proj.onrender.com'
+    'tasty-ideas-proj.onrender.com'
 ]
 
 # Application definition
