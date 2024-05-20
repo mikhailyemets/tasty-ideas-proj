822bcb837fcbc664230c2c6756c6d135d51a4852
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-05-20 19:39:54 +0200

    minor fix base_dir var

--- tasty_ideas_proj/settings.py
+++ tasty_ideas_proj/settings.py
@@ -147,7 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_ROOT = "staticfiles/"
-STATICFILES_DIRS = BASE_DIR / "static"
+STATICFILES_DIRS = [BASE_DIR / 'static',]
 STATIC_URL = "static/"
 
 
